21ce177d9d3f550d39effca6daffc1639f0cafc3
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2025-03-25 13:40:58 -0700

    Update: removed redundancy from functions, creating one function that can handle both encryption and decryption

--- cypher.py
+++ cypher.py
@@ -11,6 +11,8 @@ while message_type not in (1, 2):
     print('Incorrect input, please enter 1 or 2')
     message_type = int(input('1. Encryption\n2. Decryption\n'))
 
+direction = 1 if message_type == 1 else -1
+
 # If user enters an empty message, or a message full of spaces, they will continuously get prompted to enter a proper message
 while True:
     if message.strip():
@@ -19,36 +21,23 @@ while True:
     message = input()
 
 # For every iteration of the loop (length of the message) it first checks if the letter is in the alphabet, if so it assigns the index to the letter in the alphabet of the letter in message to a variable (alpha_index), then assigns the letter in the alphabet moved (either forwards or backwards) 'key' indicies (4), then finally adding the letter to the encrypted_message variable. If the element at message[i] is not in the alphabet (such as spaces) it gets added to encrypted_message
-def encrypt_message():
+def cypher(direction):
     encrypted_message = ''
     for i in range(len(message)):
         if message[i] in alphabet:
             alpha_index = alphabet.index(message[i])
-            encrypt_char = (alpha_index + key) % 26
+            encrypt_char = (alpha_index + (key * direction)) % 26
             encrypted_message += alphabet[encrypt_char]
         else:
             encrypted_message += message[i]
 
     return encrypted_message
-    
-def decrypt_message():
-    encrypted_message = ''
-    for i in range(len(message)):
-        if message[i] in alphabet:
-            alpha_index = alphabet.index(message[i])
-            encrypt_char = (alpha_index - key) % 26
-            encrypted_message += alphabet[encrypt_char]
-        else:
-            encrypted_message += message[i]
-
-    return encrypted_message
-
 
 # Depending on message_type, the appropriate function will run returning the message
 if message_type == 1:
-    print(f'Encrypted Message: {encrypt_message()}')
+    print(f'Encrypted Message: {cypher(direction)}')
 else:
-    print(f'Decrypted Message: {decrypt_message()}')
+    print(f'Decrypted Message: {cypher(direction)}')
 
 
 
